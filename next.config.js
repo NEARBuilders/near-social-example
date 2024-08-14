570d232d9ae27222f9cd46ff6aebef245eca08d2
jaswinder6991 <jassmultani2@gmail.com>
2024-08-15 01:12:13 +0530

    using simplified next js config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/near-social-example',
-  distDir: 'build',
   reactStrictMode: true,
 }
 
